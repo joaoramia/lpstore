9c4b320382e813a31aba970817d302390ec90c4e
joaoramia <joao.ramia@gmail.com>
2016-09-27 11:32:00 -0300

    fixing fb oauth

--- server/authentication/facebook.js
+++ server/authentication/facebook.js
@@ -31,7 +31,7 @@ var verifyCallback = function (accessToken, refreshToken, profile, done) {
                 return user.create({
                     facebook_id: profile.id,
                     name: profile.displayName,
-                    email: profile.emails ? profile.emails[0].value : profile.displayName + '@no-email.com'
+                    email: profile.emails ? profile.emails[0].value : profile.displayName.join('.') + '@no-email.com'
                 });
             }
         })
